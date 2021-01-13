bedf7c8d8a25f16b23f7a0c4f7154972cadf1b41
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-13 20:58:20 -0300

    refactor: make SignUpController conform to IController interface

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,8 @@
 import { IHttpRequest, IHttpResponse } from '../protocols/http'
+import { IController } from '../protocols/controller'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
-
-export class SignUpController {
+export class SignUpController implements IController {
   handle (httpRequest: IHttpRequest): IHttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
