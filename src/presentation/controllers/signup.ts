1ae9d90225282553c11b33232e75b73d9558585a
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-14 09:20:38 -0300

    feat: ensure SignUpController returns 500 if email validator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,6 +4,7 @@ import { badRequest } from '../helpers/http-helper'
 import { IController } from '../protocols/controller'
 import { IEmailValidator } from '../protocols/email-validator'
 import { IHttpRequest, IHttpResponse } from '../protocols/http'
+import { ServerError } from '../errors/server-error'
 
 export class SignUpController implements IController {
   private readonly emailValidator: IEmailValidator
@@ -20,11 +21,18 @@ export class SignUpController implements IController {
       }
     }
 
-    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValidEmail) {
-      return badRequest(new InvalidParamError('email'))
+    try {
+      const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValidEmail) {
+        return badRequest(new InvalidParamError('email'))
+      }
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
 
-    return badRequest(new MissingParamError(''))
+    return { statusCode: 200, body: {} }
   }
 }
