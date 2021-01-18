fbabe04c6a285480c9e2eed4abe4def3b41179c4
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-18 09:32:05 -0300

    refactor: move sut creation to factory helper methods

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,9 +1,19 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
+import { IEncrypter } from '../../data/protocols/encrypter'
 
 const valueToEncrypt = 'test.value'
 const hashedValueExpected = 'test.hashed.value'
+
+interface SutTypes {
+  sut: IEncrypter
+}
+
 const salt = 12
+const makeSut = (): SutTypes => {
+  const sut = new BcryptAdapter(salt)
+  return { sut }
+}
 
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
@@ -13,15 +23,15 @@ jest.mock('bcrypt', () => ({
 
 describe('Bcrypt Adapter', () => {
   it('Should call bcrypt with correct values', async () => {
-    const sut = new BcryptAdapter(salt)
+    const { sut } = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
 
     await sut.encrypt(valueToEncrypt)
-    expect(hashSpy).toBeCalledWith(valueToEncrypt, 12)
+    expect(hashSpy).toBeCalledWith(valueToEncrypt, salt)
   })
 
   it('Should return hashed value on success', async () => {
-    const sut = new BcryptAdapter(salt)
+    const { sut } = makeSut()
 
     const hashedValue = await sut.encrypt(valueToEncrypt)
     expect(hashedValue).toBe(hashedValueExpected)
