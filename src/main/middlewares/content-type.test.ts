ab79a964b3665d505dbd056414f2bfebddecc5b7
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-19 20:25:29 -0300

    feat: ensure requests returns default content-type as json

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content Type Middleware', () => {
+  it('Should return default content-type as json', async () => {
+    app.get('/test_content_type_json', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_content_type_json')
+      .expect('content-type', /json/)
+  })
+
+  it('Should return xml content-type when specified', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
