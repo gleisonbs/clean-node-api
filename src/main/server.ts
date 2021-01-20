67b0381ba8bbfaea4f570737dbdcff942d3d9613
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-20 20:13:53 -0300

    feat: connect to mongodb before calling app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,2 +1,11 @@
-import app from './config/app'
-app.listen(5050, () => console.log('Server running at http://localhost:5050'))
+import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+
+console.log(env.mongoUrl)
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async (): Promise<void> => {
+    const app = await (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(err => console.error(err))
