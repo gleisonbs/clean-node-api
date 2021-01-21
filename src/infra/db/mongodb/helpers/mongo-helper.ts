73e23a4dfe5e793b8882dbb2bd9ffb001d5edc57
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-21 09:28:57 -0300

    feat: ensure mongodb reconnects if connection is lost

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,7 +2,9 @@ import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
+  url: null as unknown as string,
   async connect (url: string): Promise<void> {
+    this.url = url
     this.client = await MongoClient.connect(url ?? '', {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -10,8 +12,13 @@ export const MongoHelper = {
   },
   async disconnect (): Promise<void> {
     await this.client.close()
+    this.client = null
   },
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.url)
+    }
+
     return this.client.db().collection(name)
   },
   map (collection: any): any {
