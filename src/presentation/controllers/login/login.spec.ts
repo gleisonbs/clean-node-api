16192b5493d305ced244470995acdc8cf53ff8a8
Gleison Batista Soares <gleisonbs@gmail.com>
2021-01-24 12:18:52 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,15 +2,32 @@ import { LoginController } from './login'
 import { IHttpRequest } from '../../protocols'
 import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors'
+import { IEmailValidator } from '../signup/signup-protocols'
+
+const email = 'test.user@email.com'
+const password = 'test.password'
+
+const makeEmailValidator = (): IEmailValidator => {
+  class EmailValidatorStub implements IEmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  return new EmailValidatorStub()
+}
 
 interface SutTypes {
   sut: LoginController
+  emailValidator: IEmailValidator
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new LoginController()
+  const emailValidator = makeEmailValidator()
+  const sut = new LoginController(emailValidator)
   return {
-    sut
+    sut,
+    emailValidator
   }
 }
 
@@ -20,7 +37,7 @@ describe('Login Controller', () => {
 
     const httpRequest: IHttpRequest = {
       body: {
-        password: 'test.password'
+        password
       }
     }
 
@@ -33,11 +50,26 @@ describe('Login Controller', () => {
 
     const httpRequest: IHttpRequest = {
       body: {
-        email: 'test.user@email.com'
+        email
       }
     }
 
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  it('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidator } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidator, 'isValid')
+
+    const httpRequest: IHttpRequest = {
+      body: {
+        email,
+        password
+      }
+    }
+
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toBeCalledWith(email)
   })
 })
