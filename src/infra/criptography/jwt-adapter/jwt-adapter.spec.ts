6a825cfcfe7b563b039816e5053f983cbc7c16db
Gleison Batista Soares <gleison.batista@fs.com.br>
2021-01-29 09:33:55 -0300

    test: ensure JwtAdapter returns a token on sign success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -4,6 +4,12 @@ import { JwtAdapter } from './jwt-adapter'
 const userId = 'test.user.id'
 const secret = 'secret'
 
+jest.mock('jsonwebtoken', () => ({
+  sign: async (): Promise<string> => {
+    return 'test.accessToken'
+  }
+}))
+
 describe('JWT Adapter', () => {
   it('Should call sign with correct paramters', async () => {
     const sut = new JwtAdapter(secret)
@@ -11,5 +17,12 @@ describe('JWT Adapter', () => {
 
     await sut.encrypt(userId)
     expect(signSpy).toBeCalledWith({ id: userId }, secret)
+  })
+
+  it('Should return a token on sign success', async () => {
+    const sut = new JwtAdapter(secret)
+
+    const accessToken = await sut.encrypt(userId)
+    expect(accessToken).toBeTruthy()
   })
 })
