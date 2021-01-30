0666a50f4f6eb0aef5748432eef80f9b293b6168
Gleison Batista Soares <gleison.batista@fs.com.br>
2021-01-30 12:31:40 -0300

    fix: fix protocol import

--- src/data/use-cases/add-account/db-add-account-protocols.ts
+++ src/data/use-cases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/use-cases/add-account'
-export * from '../../protocols/db/add-account-repository'
+export * from '../../protocols/db/account/add-account-repository'
 export * from '../../../domain/models/account'
 export * from '../../protocols/criptography/hasher'
