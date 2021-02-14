e8798e35d56a5f3aba2e8c2c841e2d53ff888ab7
Gleison Batista Soares <gleisonbs@gmail.com>
2021-02-14 15:24:04 -0300

    test: ensure AuthMiddleware returns 500 if LoadAccountByToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { IMiddleware } from '../protocols/middleware'
 import { IHttpRequest, IHttpResponse } from '../protocols'
-import { forbidden, ok } from '../helpers/http/http-helper'
+import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { ILoadAccountByToken } from '../../domain/use-cases/load-account-by-token'
 
 export class AuthMiddleware implements IMiddleware {
@@ -9,13 +9,17 @@ export class AuthMiddleware implements IMiddleware {
   }
 
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const accessToken = httpRequest.headers['x-access-token']
-    if (accessToken) {
-      const account = await this.loadAccountByToken.load(accessToken)
-      if (account) {
-        return ok({ accountId: account.id })
+    try {
+      const accessToken = httpRequest.headers['x-access-token']
+      if (accessToken) {
+        const account = await this.loadAccountByToken.load(accessToken)
+        if (account) {
+          return ok({ accountId: account.id })
+        }
       }
+      return forbidden()
+    } catch (err) {
+      return serverError(err)
     }
-    return forbidden()
   }
 }
