c65c9271d0a95199e4e8a969d67e6c41aeb7b4bf
Gleison Batista Soares <gleisonbs@gmail.com>
2021-02-21 14:59:45 -0300

    feat: ensure LoadSurveysRepository loads all surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,17 @@
 import { IAddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
 import { IAddSurveyModel } from '../../../../domain/use-cases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { ISurveyModel } from '../../../../domain/models/survey'
 
 export class SurveyMongoRepository implements IAddSurveyRepository {
   async add (surveyData: IAddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<ISurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys: ISurveyModel[] = await surveyCollection.find().toArray()
+    return surveys
   }
 }
